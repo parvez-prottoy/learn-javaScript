229e3ed629dd2d2a2c44a32197f90968113b43dd
parvezAhmedWEB <parvezahmedprottoy.web@gmail.com>
2024-03-21 09:26:43 +0600

    added array part 01 and 02

---
+++ array/01.js
@@ -0,0 +1,8 @@
+/**
+ * ! Array Literal
+ */
+const numbers = [1, 2, 3, 4, 5];
+// How to get arr length
+console.log(numbers.length);
+// How to get arr last index
+console.log(numbers.length - 1);
---
+++ array/02.js
@@ -0,0 +1,9 @@
+/**
+ * ?@Title: Initialize Array - Constructor vs Factory
+ */
+// ! Constructor
+const a1 = new Array(); // []
+const a2 = new Array(5); // a2 arr data limit 5
+// ! Factory
+const b1 = Array(); // []
+const b2 = Array(5); // a2 arr data limit 5
